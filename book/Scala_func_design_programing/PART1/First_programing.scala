7be17e7942a3bb63440f6eea918b5c7afc8a266f
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-04-16 16:03:47 +0900

    Add if constructor

--- book/Scala_func_design_programing/PART1/First_programing.scala
+++ book/Scala_func_design_programing/PART1/First_programing.scala
@@ -1,5 +1,8 @@
 
 object FirstPrograming{
+  def abs(target: Int): Int = 
+    if (target > 0) target
+    else  -target
   def main(args: Array[String]) : Unit = {
     val x = new StringBuilder("Hello, World")
     val y = x.append(", world")
